eddb85101c3a90b87385e218074fa6e058993c46
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-07-28 17:27:49 +0900

    마이페이지 게시글 수정

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path,re_path
-from . import views 
+from django.urls import path, re_path
+from . import views
 from .views import *
 from django.contrib.auth.decorators import login_required
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "mypage"
 urlpatterns = [
-    path('mypage/<int:id>',views.mypage,name='mypage'),
-    path('profile_update/', login_required(views.ProfileUpdateView.as_view()), name='profile_update'),
-    path('<int:id>/follow',views.follow,name="follow"),
+    path('mypage/<int:id>', views.mypage, name='mypage'),
+    path('profile_update/', login_required(views.ProfileUpdateView.as_view()),
+         name='profile_update'),
+    path('<int:id>/follow', views.follow, name="follow"),
+
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
